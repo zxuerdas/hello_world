ee64f033e9cc19203c15be96277c93af73749745
zxuerdas <zxu.jeff@gmail.com>
2020-06-21 15:09:14 -0400

    Add files via upload

---
+++ course3_wk1.py
@@ -0,0 +1,21 @@
+# n = input("Please enter your age: ")
+# # user types in 18
+# print(type(n))
+
+
+p_phrase = "was it a car or a cat I saw"
+
+r_phrase = reversed(p_phrase)
+r_phrase = "".join(list(r_phrase))
+if r_phrase == p_phrase:
+    print("It is palindrome")
+else:
+    print("It is not")
+
+    print("////////////////////////////")
+list1= [5, 2, 1, 4, 9, 10]
+min_value = list1[0]
+for item in list1:
+   if item < min_value:
+       min_value = item
+print(min_value)
